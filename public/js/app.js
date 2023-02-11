4dedbecb5c97035e4da20b95d48dac7f062dd6a9
DivyanshChoukse <divyanshchoukse443@gmail.com>
2023-02-11 15:44:21 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log("Client side javascript file is loaded");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.getElementById("message-1");
+const messageTwo = document.getElementById("message-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  messageOne.innerText = "Loading...";
+  messageTwo.innerText = "";
+  e.preventDefault();
+  const location = search.value;
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.innerText = data.error;
+        } else {
+          messageOne.innerText = data.location;
+          messageTwo.innerText = data.forecast;
+        }
+      });
+    }
+  );
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require("request");
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "http://api.weatherstack.com/current?access_key=052aa78971bef7228ce69252c80d13bf&query=" +
+    longitude +
+    "," +
+    latitude;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service", undefined);
+    } else if (body.success === false) {
+      callback("Unable to find the location", undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}.It is currently ${body.current.temperature} out there and its feels like ${body.current.feelslike}`
+      );
+    }
+  });
+};
+module.exports = forecast;
